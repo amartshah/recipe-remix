2e8bc4e90d4f7032bceda6e374d85b869a6ca6b0
collin <barnwell.collin@gmail.com>
2016-03-02 20:44:53 -0600

    added autograder, parser.py structure

--- parser.py
+++ parser.py
@@ -1,4 +1,19 @@
+class Parser():
+    def __init__(self, url):
+        extract_recipe(url)
 
+    def parse_html(self, url):
+        pass
 
-def extract_recipe(url):
-    pass
+    def extract_recipe(self, url):
+        (ingredients, steps, cook_time, prep_time) = parse_html(url)
+        self.ingredients = ingredients
+        self.steps = steps
+        self.cook_time = cook_time
+        self.prep_time = prep_time
+
+    def separate_ingredients(self):
+        pass
+
+    def get_tools(self):
+        pass
