fa5ab11a6ca3dbe642c8f52c61b39a416d70f977
LiamFlahive <williamflahive2012@u.northwestern.edu>
2016-03-10 18:34:33 -0600

    ingredients
    pushing ingredients

---
+++ Team0/ingredients.py
@@ -0,0 +1,102 @@
+import json
+import nltk
+import operator
+import sys
+import re
+
+test = ['2/3 large eggs, evenly separated', '5 1/4 teaspoon cream of tartar', '2 ounces cream cheese, very soft', '1 tablespoon white sugar', '1 dash hot pepper sauce (such as Frank\'s RedHot), or to taste']
+measurements = ['teaspoon', 'cup', 'tablespoon', 'pound', 'ounce', 'liter', 'gallon', 'quart', 'pint', 'milliliter', 'pinch', 'handful', "dash", 'to taste']
+
+def remove_parens(st):
+    #parens = re.compile(r'"([A-Za-z0-9_\./\\-]*)"')
+    return re.sub(r'\s?\(.*?\)', '', st)
+
+def find_name(st):
+    start = re.split(r',',st)[0];
+    name = re.split(r'\s', start)[-1];
+    #print name;
+    return name;
+
+def find_quant(st):
+    #pattern = r'[0-9]([0-9/\s]*)[0-9]'
+    match = re.search(r'[0-9]([0-9/\s]*)[0-9]|[0-9]', st)
+    #longest_match = max(matches)
+    if match is not None:
+        num = match.group();
+        if '/' in num:
+            whole = 0.0
+            frac = 0.0
+            wmatch = re.match(r'^[0-9]*\s', st)
+            if wmatch is not None:
+                whole = float(wmatch.group()[0]);
+            denom = float(re.split('/', num)[1])
+            numer = float(re.split('/', num)[0][-1])
+            #print denom
+            frac = round(whole + (numer/denom),3)
+            #print frac
+            return frac
+        else:
+            return int(num)
+    else:
+        return None
+
+def find_measurement(st):
+    for meas in measurements:
+        match = re.search(r''+meas+'+?', st);
+        if match is not None:
+            #print match.group()
+            return meas
+    return "discrete"
+
+def find_descriptor(st, name, meas):
+    start = re.split(r''+name+'',st)[0];
+    #print start
+    middle = re.split(r''+meas+'s|'+meas+'', start)[-1];
+    #print middle
+    desc = re.split(r'[0-9]\s', middle)[-1];
+    #print desc
+    if desc[0] is ' ':
+        desc = desc[1:]
+    if desc[-1] is ' ':
+        size = len(desc)-1
+        desc = desc[:size]
+    #print desc
+    return desc      
+
+def find_prep(st):
+    lastW = re.split(r'\s',st)[-1];
+    match = re.match(r'.*ed$', lastW)
+    if match is not None:
+        #print match.group()
+        return match.group()
+    else:
+        return None  
+
+def find_prep_descriptor(st):
+    words = re.split(r'\s',st);
+    check = words[len(words)-2];
+    match = re.match(r'.*ly$', check)
+    if match is not None:
+        #print match.group()
+        return match.group()
+    else:
+        return None           
+
+def findIngredients(ingList):
+    parsedIngs = []
+    for ing in ingList:
+        ing = remove_parens(ing)
+        #print ing
+        name = find_name(ing)
+        meas = find_measurement(ing)
+        desc = find_descriptor(ing, name, meas)
+        prep = find_prep(ing);
+        prepD = None
+        if prep is not None:
+            prepD = find_prep_descriptor(ing)
+        ingDic = {"name": name, "quantity": find_quant(ing), "measurement": meas, "descriptor": desc, "preparation": prep, "prep-description": prepD}
+        parsedIngs.append(ingDic);
+    print parsedIngs
+    return parsedIngs
+
+#findIngredients(test)
--- Team0/parser.py
+++ Team0/parser.py
@@ -1,14 +1,15 @@
 from recipeparser import parseHtml
 from cooking_methods import METHODS
 from tools import toolFinder
+from ingredients import findIngredients
 
 class Parser():
     def __init__(self, url):
         self.url = url
         parse_html()
 
-    # def __package__(self):
-    #     return self.__name__
+    def __package__(self):
+        return self.__name__
 
     def parse_html(self):
         parsed = parseHtml(self.url)
@@ -18,14 +19,15 @@ class Parser():
         self.prep_time = parsed["prep time"]
         self.name = parsed["name"]
 
+
     def separate_ingredients(self):
-        pass
+        return findIngredients(self.ingredients)
 
     def get_tools(self):
-        return toolFinder(instructions)
+        return toolFinder(self.steps)
 
     def primary_cooking_method(self):
-
+        pass
     def cooking_methods(self):
         self.methods = [m for m in METHODS if m in self.name.lower()]
         if self.methods == []:
@@ -38,7 +40,7 @@ class Parser():
         if not self.full_recipe:
             self.full_recipe = {
                 "url": self.url,
-                "ingredients": self.separate_ingredients,
+                "ingredients": self.separate_ingredients(),
                 "primary cooking method": self.primary_cooking_method(),
                 "cookings methods": self.cooking_methods(),
                 "cooking tools": self.get_tools()
