061d505da755b89dfe4bb5eb536b0cb984445c86
collin <barnwell.collin@gmail.com>
2016-03-13 18:06:33 -0500

    created style shifter for asian food

---
+++ Team0/change_style.py
@@ -0,0 +1,65 @@
+from style_transformations import *
+import copy
+
+
+def change_style(transformations, ingredients, steps):
+    new_ingredients_list = []
+    for i in ingredients:
+        new_ingredient = transformations.get(i['name'])
+        if new_ingredient:
+            formatted_ingredient = None
+            if isinstance(list, new_ingredient):
+                # measurement conversion
+                formatted_ingredient = copy.deepcopy(i)
+                formatted_ingredient['name'] = new_ingredient[0]
+                if not new_ingredient[1]:
+                    # remove descriptors, etc.
+                    formatted_ingredient['descriptor'] = 'none'
+                    formatted_ingredient['preparation'] = 'none'
+                    formatted_ingredient['prep-description'] = 'none'
+            else:
+                # measurement conversion
+                measurement_mapping = new_ingredient.get(i['measurement'))
+                if measurement_mapping:
+                    formatted_ingredient= {
+                        'name': measurement_mapping[2],
+                        'quantity': measurement_mapping[0] * i['quantity'],
+                        'measurement': measurement_mapping[1],
+                        'descriptor': 'none',
+                        'preparation': 'none',
+                        'prep-description': 'none'
+                    }
+                if not measurement_mapping and new_ingredient.get('other'):
+                    formatted_ingredient = copy.deepcopy(i)
+                    formatted_ingredient['name'] = new_ingredient.get('other')
+
+            if formatted_ingredient is not None:
+                new_ingredients_list.append(formatted_ingredient)
+                steps = [step.replace(i['name'], formatted_ingredient['name']) for step in steps]
+
+        else:
+            new_ingredients_list.append(i)
+
+    return [new_ingredients_list, steps]
+
+
+def change_style_to(new_style, ingredients, steps):
+    if new_style == 'asian':
+        transformations = to_asian
+
+    if not transformations:
+        raise ValueError('Transformation parameter not recognized')
+
+    return change_style(transformations, ingredients, steps)
+
+
+
+# ingredient format (for reference)
+# {
+#     "name":	"salt",
+#     "quantity":	1,
+#     "measurement":	"pinch",
+#     "descriptor":	"table",
+#     "preparation":	"none",
+#     "prep-description":	"none"
+# }
---
+++ Team0/style_transformations.py
@@ -0,0 +1,64 @@
+to_asian = {
+    'noodles': {
+        'cup': [1, 'cup', 'rice']
+        'other': 'rice noodles'
+    },
+    'salt': {
+        'teaspoon': [2, 'tablespoon', 'soy sauce'],
+        'tablespoon': [4, 'tablespoon', 'soy sauce']
+    },
+    'mozerella cheese': {
+        'cup': [1, '', 'poached or soft-boiled egg'],
+    },
+    'cheddar cheese': {
+        'cup': [1, '', 'poached or soft-boiled egg'],
+    },
+    'parmesan cheese': {
+        'cup': [1, '', 'poached or soft-boiled egg'],
+    },
+    'sugar': {
+        'tablespoon': [1, 'tablespoon', 'teriyaki sauce'],
+        'teaspoon': [1, 'teaspoon', 'teriyaki sauce'],
+        'cup': [2, 'tablespoon', 'hoisin sauce']
+    },
+
+    'worcestershire sauce': ['hoisin sauce', True],
+
+    'cilantro': ['basil', True],
+    'parsley': ['basil', True],
+    'milk': ['coconut milk', True],
+
+    'olive oil': ['sesame oil', False],
+    'vegetable oil': ['sesame oil', False],
+
+    'brussels sprouts': ['bok choy', True],
+    'potatoes': ['drained 5 oz. can of water chestnuts', False],
+    'potato': ['drained 5 oz. can water chestnuts', False],
+    'artichokes': ['drained 5 oz. can of bamboo shoots', True],
+    'artichoke hearts': ['bamboo shoots', True],
+    'artichoke': ['drained 5 oz. can bamboo shoots', False],
+
+    'cumin': ['ground ginger', True],
+    'paprika': {
+        'teaspoon': [1, 'tablespoon', 'garlic chili sauce']
+    },
+
+    'oregano': {
+        'teaspoon': [.5, 'teaspoon', 'ground ginger'],
+        'tablespoon': [1, 'teaspoon', 'ground ginger']
+    },
+    'horseradish': ['minced ginger', False],
+    'pickles': ['pickled ginger', False]
+}
+
+# to_mex = {
+
+# }
+
+# to_italian = {
+
+# }
+
+# to_mediterranean = {
+
+# }
